8c8581497e610cdcf9e0ee44cc38d1d18f62c4a1
12Shivam12 <120035361+12Shivam12@users.noreply.github.com>
2023-09-16 19:29:35 +0530

    basic functionality added

--- task_list/src/App.js
+++ task_list/src/App.js
@@ -1,13 +1,11 @@
 import { Button, } from '@chakra-ui/react'
 import './App.css';
 import TaskList from './components/Task-List';
-import TodoApp from './components/TodoApp';
 
 function App() {
   return (
     <div className="App">
       <TaskList/>
-      {/* <TodoApp/> */}
     </div>
   );
 }
--- task_list/src/components/Task-List.jsx
+++ task_list/src/components/Task-List.jsx
@@ -1,7 +1,7 @@
 
 import React, { useEffect, useState } from 'react'
 import './Styles/taskList.css'
-import { Heading, Button, Input, Checkbox, Flex, Center, Spacer, useToast, Box, textDecoration } from '@chakra-ui/react'
+import { Heading, Button, Input, Checkbox, Flex, Center, Spacer, useToast, Box, textDecoration, Select } from '@chakra-ui/react'
 import { DeleteIcon, EditIcon, ViewIcon, } from '@chakra-ui/icons'
 
 
@@ -17,10 +17,15 @@ const TaskList = () => {
     const [taskInput, setTaskInput] = useState("");
     const [isChecked, setIsChecked] = useState("");
     const [editIndex, setEditindex] = useState(null);
+    const [filterValue,setfilterValue] = useState("");
+    const [origninalTask,setOriginalTask] = useState([]);
 
     const toast = useToast()
 
 
+    useEffect(() =>{
+        applyFilter();
+    },[filterValue])
 
     const handleSubmit = () => {
         if (taskInput.trim() !== "") {
@@ -29,6 +34,7 @@ const TaskList = () => {
                 isCompleted: false
             }
             setTask([...task, obj])
+            setOriginalTask([...task,obj])
             setTaskInput("")
 
         } else {
@@ -41,6 +47,19 @@ const TaskList = () => {
         }
 
     }
+
+    const applyFilter = () =>{
+        let filteredArr = [...origninalTask]
+        if(filterValue === "completed"){
+             filteredArr = filteredArr.filter((value) => value.isCompleted === true)
+            
+        }else if(filterValue === "incompleted"){
+             filteredArr = filteredArr.filter((value) => value.isCompleted === false)
+            
+        }
+        setTask(filteredArr)
+    }
+    
 
     const handleDelete = (index) => {
         let updatedArr = [...task]
@@ -59,7 +78,7 @@ const TaskList = () => {
     }
 
 
-    const handleCheckbox = (index) =>{
+    const handleCheckbox = (index) => {
         let updatedTasks = [...task];
         updatedTasks[index].isCompleted = !updatedTasks[index].isCompleted;
         setTask(updatedTasks);
@@ -69,54 +88,72 @@ const TaskList = () => {
         const updatedTasks = [...task];
         updatedTasks[index].text = newText;
         setTask(updatedTasks);
-      };
+    };
 
 
     return (
         <div className='parentDiv'>
-            <div className='taskList'>
-                <Heading mt='10px' mb='15px'>Task-List App</Heading>
-                <div className='addTask'>
-                    <Input placeholder='Add Task...' size='sm' width='auto' value={taskInput} onChange={(e) => setTaskInput(e.target.value)}></Input>
-                    <Button bg='blue.300' onClick={handleSubmit}>Submit</Button>
-                </div>
-                <div className='displayTask'>
-
-
-                    {
-                        task.map((ele, index) => {
-
-                            return (editIndex === index) ?
-                                (
-                                    <div className='tasks' key={index}>
-                                       
-                                    </div>
-                                ) : (
-                                    <div className='tasks' key={index}>
-                                        <div>
-                                            <Heading  size='md'><span style={{textDecoration: (ele.isCompleted) ? "line-through" : "none"}}>{ele.text}</span></Heading>
-                                        </div>
-                                       
-                                        <div>
-                                            <Flex gap='5'>
-                                                <Checkbox colorScheme='green' isChecked={ele.isCompleted} onChange={() => handleCheckbox(index)} size='lg' />
-                                                <EditIcon onClick={() => {
-                                                    const newText = prompt('Edit task:', ele.text);
-                                                    if (newText !== null) {
-                                                        editTask(index, newText);
-                                                    }
-                                                }} className='pointer' boxSize={6} color='green.500'></EditIcon>
-                                                <DeleteIcon onClick={() => handleDelete(index)} className='pointer' boxSize={6} color='red.500'></DeleteIcon>
-                                            </Flex>
-                                        </div>
-                                    </div>
-                                )
-                        })
-                    }
-
-                </div>
+        <div className='taskList'>
+          <Heading mt='10px' mb='30px' color={"red.400"} fontSize={['xl', '2xl', '3xl']}>Task-List App</Heading>
+          <div className='addTask'>
+            <div>
+              <Input
+                placeholder='Add Task...'
+                textColor={"blue.600"}
+                fontWeight={"xl"}
+                size={['xs', 'sm', 'md']} // Adjust size for different screen sizes
+                width={['100%', 'auto']} // Adjust width for different screen sizes
+                value={taskInput}
+                onChange={(e) => setTaskInput(e.target.value)}
+              ></Input>
+              <Button size={['xs', 'sm']} bg='blue.300' onClick={handleSubmit}>Submit</Button> {/* Adjust button size */}
             </div>
+            <div>
+              <Select
+                placeholder='Select option'
+                value={filterValue}
+                onChange={(e) => setfilterValue(e.target.value)}
+                size={['xs', 'sm']} // Adjust size for different screen sizes
+              >
+                <option value='all'>All</option>
+                <option value='completed'>Completed</option>
+                <option value='incompleted'>Incompleted</option>
+              </Select>
+            </div>
+          </div>
+          <div className='displayTask'>
+            <ul>
+              {
+                task.map((ele, index) => (
+                  (editIndex === index) ?
+                    (
+                      <div className='tasks' key={index}></div>
+                    ) : (
+                      <div className='tasks' key={index}>
+                        <div>
+                          <Heading size={['sm', 'md']}><span style={{ textDecoration: (ele.isCompleted) ? "line-through" : "none" }}>{ele.text}</span></Heading>
+                        </div>
+                        <div>
+                          <Flex gap={['2', '5']}>
+                            <Checkbox colorScheme='green' isChecked={ele.isCompleted} onChange={() => handleCheckbox(index)} size={['sm', 'lg']} /> {/* Adjust checkbox size */}
+                            <EditIcon color="blue.600" onClick={() => {
+                              const newText = prompt('Edit task:', ele.text);
+                              if (newText !== null) {
+                                editTask(index, newText);
+                              }
+                            }} className='pointer' boxSize={['4', '6']} /> {/* Adjust icon size */}
+                            <DeleteIcon onClick={() => handleDelete(index)} className='pointer' boxSize={['4', '6']} color='red.600' /> {/* Adjust icon size */}
+                          </Flex>
+                        </div>
+                      </div>
+                    )
+                ))
+              }
+            </ul>
+          </div>
         </div>
+      </div>
+
     )
 
 }
